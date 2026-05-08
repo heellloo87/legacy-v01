82e6cd3be33cfda74f1ac5bbd637d1e8f9b43e89
heellloo87 <heellloo87@gmail.com>
2026-05-08 22:52:46 +0500

    Create usePermissions.ts

---
+++ src/hooks/usePermissions.ts
@@ -0,0 +1,10 @@
+import { useAuth } from "@/lib/auth";
+import { rolePermissions } from "@/lib/permissions";
+
+export function usePermissions() {
+  const { role } = useAuth();
+
+  return rolePermissions[
+    role ?? "designer"
+  ];
+}
